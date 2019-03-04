2ce6ff2404854a943348bbaf0840d0ce7caed348
Timo Toots <info@timo.ee>
2019-03-04 11:22:50 +0200

    Converter refactor.

--- backend/converter/converter.py
+++ backend/converter/converter.py
@@ -1,11 +1,14 @@
 #!/usr/local/bin/python
 
 # Libraries
-import ujson, urllib3, certifi, pandas, ijson
+import ujson, urllib3, certifi, pandas, sys, json, decimal
+import ijson.backends.yajl2_cffi as ijson
 import json_schema_generator, threading
 import xml.etree.ElementTree as ET
 from json_schema_generator.generator import SchemaGenerator
 from sqlalchemy import create_engine
+from geoalchemy2 import Geometry, WKTElement
+from geomet import wkt
 
 # Constants
 CONFIG_FILE = '../config/config.json'
@@ -98,6 +101,107 @@ def schematize(f):
   except Exception as e:
     print "Cannot schematize file", path, e 
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
+
+def populate_dict_from_json(record, field_description, dictionary):
+  # If there is a field, keep the data
+  if "field" in field_description: 
+    fieldname = field_description['field']
+    # If field is separated by "." we recursively go in the tree of the record
+    if len(fieldname.split(".")) > 1:
+      tmp = record
+      fieldhierarchy = fieldname.split(".")
+      for i in fieldhierarchy:
+        if i in tmp:
+          tmp=tmp[i]
+
+      # we did not find anything
+      if tmp is record:
+        tmp = ""
+        
+      # If the type of the field is text, write content in field,
+      # either underlying JSON or text content and change column name
+      if field_description["type"] == 'text':
+        try:
+          if type(tmp) is unicode:
+            text_value = tmp.encode("utf8")
+          else:
+            text_value= str(tmp).encode("utf-8")
+          dictionary[field_description["column"]] = [text_value]
+        except BaseException as e:
+          print(e)
+    else:
+      value = [record[fieldname]]
+      # if field_description["type"] == "geojson":
+        # value = json.dumps(record[fieldname], cls=DecimalEncoder)
+      # If field is without "." and contained in record,
+      # write it to dict and change column name
+      if fieldname in record:
+        dictionary[field_description["column"]] = [value]
+      else:
+        dictionary[field_description["column"]] = [""]
+
+    # If there is no field, run given custom script to build it
+  else:
+    mod = __import__("scripts." + field_description["script"])
+    function = getattr(getattr(mod, field_description["script"]), field_description["script"])
+    field = function(record)
+    dictionary[field_description["column"]] = [field]
+  return dictionary
+  
+from shapely.geometry import shape
+    
+def handle_json_records(records, f, engine):
+  data = pandas.DataFrame()
+  first = True
+  counter = 0
+
+  def write_pandas_df(data):
+    if f["type"] == "geojson":
+      def to_wkt(x):
+        return wkt.dumps(x[0])
+      
+      data['geom'] = data['geometry'].apply(to_wkt) 
+      data.drop('geometry', 1, inplace=True)
+      
+    if first:
+      data.to_sql(f["id"], engine, if_exists='replace', chunksize=10000, dtype={'geom': Geometry('', srid=4326)})
+      # Append dataframe to db
+    else:
+      data.to_sql(f["id"], engine, if_exists='append', chunksize=10000, dtype={'geom': Geometry('', srid=4326)})
+    
+  # Loop throught every record in the JSON
+  for o in records:
+    print(counter)
+    dictionary = {}       # will hold data from record in dict form
+    schema = f["schema"]  # Mapping for columns
+
+    if f["type"] == "geojson":
+      schema.append({"column": "geometry", "type" :"geojson", "field": "geometry"})
+    # Loop through every column to map
+    for k in schema:
+      populate_dict_from_json(o, k, dictionary)
+      
+    chunk = pandas.DataFrame.from_dict(dictionary)
+    data = pandas.concat([data, chunk], sort=False, join='outer')
+
+    if counter != 100:
+      counter += 1
+      
+    else:
+      counter = 0
+      write_pandas_df(data)
+      data = pandas.DataFrame()
+      first = False
+  # write leftover data if any
+  if not data.empty:
+    write_pandas_df(data)
+  
+    
 # Insert given file to postgreSQL
 def convert_and_insert_DB(f, engine):
   # Construct path with file object
@@ -109,79 +213,15 @@ def convert_and_insert_DB(f, engine):
   try:
     if filetype == 'json' or filetype == 'geojson':
       # JSON stream parsing, going from schema given top element
-      objects = ijson.items(open(path), f["converter_top_element"])
+
+      prefix = f["converter_top_element"]
+      if prefix:
+        prefix += ".item"
+      else:
+        prefix = "item"
+      objects = ijson.items(open(path), prefix)
+      handle_json_records(objects, f, engine)
       
-      # Library specific loop
-      for o in objects:
-        first = True
-        counter = 0
-        
-        # Loop throught every record in the JSON
-        for record in o:
-          dictionary = {}       # will hold data from record in dict form
-          schema = f["schema"]  # Mapping for columns
-          
-          # Loop through every column to map
-          for k in schema:
-            
-            # If there is a field, keep the data
-            if "field" in k: 
-              # If field is separated by "." we recursively go in the tree of the record
-              if len(k["field"].split(".")) > 1:
-                tmp = record
-                fieldhierarchy = k["field"].split(".")
-                for i in fieldhierarchy:
-                  if i in tmp:
-                    tmp=tmp[i]
-
-                # If the type of the field is text, write content in field,
-                # either underlying JSON or text content and change column name
-                if k["type"] == 'text':
-                  dictionary[k["column"]] = [str(tmp)]
-              else:
-
-                # If field is without "." and contained in record,
-                # write it to dict and change column name
-                if k["field"] in record:
-                  dictionary[k["column"]] = [record[k["field"]]]
-                else:
-                  dictionary[k["column"]] = [""]
-            
-            # If there is no field, run given custom script to build it
-            else:
-              mod = __import__("scripts." + k["script"])
-              function = getattr(getattr(mod, k["script"]), k["script"])
-              field = function(record)
-              dictionary[k["column"]] = [field]
-
-          # Every 100 record, parse it to pandas dataframe
-          if counter != 100:
-            chunk = pandas.DataFrame.from_dict(dictionary)
-            data = pandas.concat([data, chunk], sort=False, join='outer')
-            counter += 1
-          
-          # Last record, parse rest of the data if there is some         
-          elif counter == len(o):
-            
-            # If total number of records is smaller than 100,
-            # first time writing to db has to replace table
-            if first:
-              data.to_sql(f["id"], engine, if_exists='replace', chunksize=10000)
-              first = False
-            
-            # Append dataframe to db
-            else:
-              data.to_sql(f["id"], engine, if_exists='append', chunksize=10000)
-          else:
-              
-            # First time writing to db has to replace table
-            if first:
-              data.to_sql(f["id"], engine, if_exists='replace', chunksize=10000) 
-              first = False
-            
-            # Append dataframe to db
-            else:
-              data.to_sql(f["id"], engine, if_exists='append', chunksize=10000)
 
     elif filetype == 'csv':
       # Read and add CSV to DB by chunks 
@@ -200,6 +240,7 @@ def convert_and_insert_DB(f, engine):
       print "Bad file type", f["type"]
   except Exception as e:
     print "Cannot convert file", path, e
+    raise (e)
     pass
 
 # Run file through pipeline
@@ -216,10 +257,8 @@ if __name__ == '__main__':
   # Read config file and fetch file object for each dataset
   config = ujson.load(open(CONFIG_FILE))
   dataset_json = ujson.load(open(DATASETS_FILE))
-  datasets = []
-  for i in dataset_json:
-    datasets.append(i['datasets'])
-  
+  datasets = [ d['datasets'] for d in dataset_json ]
+
   # PostgreSQL engine
   engine = create_engine("postgresql://" + config["database_user"] + ":" + config["database_password"] + "@localhost/" + config["database_name"])
   
@@ -228,6 +267,10 @@ if __name__ == '__main__':
   threads = {}
   for ds in datasets: 
     for f in ds:
+      if len(sys.argv) > 1:
+        if f['id'] not in sys.argv:
+          continue
+
       # entry for the thread is the function pipeline(f, engine)
       t = threading.Thread(target=pipeline, args=(f, engine)) 
       threads[f["id"]] = t
