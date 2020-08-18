154f591aa649bc009b3cd16e82749f06a9dae8bd
Tanel Karindi <mou@arvuti>
2020-08-18 22:42:05 +0300

    Do not use HTTP keepalive when setting up scrapykeeper's project.

--- scrapykeeper/init_scrapykeeper.sh
+++ scrapykeeper/init_scrapykeeper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --post-data=project_name=Laastutabloo  http://$scrapykeeper_user:$scrapykeeper_password@localhost:5050/api/projects
+wget --post-data=project_name=Laastutabloo  http://$scrapykeeper_user:$scrapykeeper_password@localhost:5050/api/projects --no-http-keep-alive
