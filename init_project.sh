46285cce6c595ee0f6a349f8f5b856b6bdfc861c
Tanel Karindi <mou@arvuti>
2020-08-18 16:01:09 +0300

    Wait for db to start before running startup scripts.

--- init_project.sh
+++ init_project.sh
@@ -7,11 +7,17 @@ mkdir -p backend/data/logs
 
 docker-compose up --no-start
 docker-compose start postgres
-sleep 1
+until docker-compose exec postgres psql -U postgres  -V;
+do
+    sleep 1;
+    echo "Waiting for Postgres container to start."
+done
+
+docker-compose exec postgres psql -U postgres  -V
 docker-compose exec postgres su -c /opt/laastutabloo/config/postgres_startup.sh  postgres 
 
 docker-compose start datastore scrapyd scrapykeeper
-sleep 1
+sleep 3
 docker-compose exec scrapyd scrapyd-deploy
 docker-compose exec scrapykeeper /init_scrapykeeper.sh
 docker-compose run datastore python3 /opt/laastutabloo/backend/datastore/dataset_json_to_db_loader.py --providers datasets2 --input /output
