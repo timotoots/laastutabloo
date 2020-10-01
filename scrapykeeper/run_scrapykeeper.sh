e93ebec50c6f49fc50a3db38771766cb29664f06
Tanel Karindi <mou@arvuti>
2020-10-01 15:50:07 +0300

    Scrapykeeper uses Postgres database.

--- scrapykeeper/run_scrapykeeper.sh
+++ scrapykeeper/run_scrapykeeper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scrapykeeper --port 5050 --server http://scrapyd:6800 --username $scrapykeeper_user --password  $scrapykeeper_password
+scrapykeeper --port 5050 --server http://scrapyd:6800 --username $scrapykeeper_user --password  $scrapykeeper_password --database-url=postgres://$laastutabloo_db_user:$laastutabloo_db_password@postgres:5432/scrapykeeper
 
